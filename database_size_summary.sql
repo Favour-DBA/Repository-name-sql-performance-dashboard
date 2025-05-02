44fed37c9aa2a20285273fa83f7e06cfcc8582b7
Favour-DBA <154571954+Favour-DBA@users.noreply.github.com>
2025-05-02 11:32:32 -0400

    Replace  faulty database size script with working version

--- database_size_summary.sql
+++ database_size_summary.sql
@@ -1,8 +1,7 @@
--- Database Size Summary for All Databases
 EXEC sp_MSforeachdb 
-'USE [?]; 
+'USE [?];
 SELECT 
-    DB_NAME() AS DatabaseName, 
-    SUM(size * 8 / 1024) AS SizeMB 
-FROM sys.master_files 
-GROUP BY DB_NAME();';
+    ''?'' AS DatabaseName,
+    SUM(size * 8 / 1024) AS SizeMB
+FROM sys.master_files
+GROUP BY DB_NAME(database_id);'
